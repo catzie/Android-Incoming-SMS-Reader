9e13b68a3921fa9e5207863d57106a75cdec25a9
Catzie (MacMini) <i@catzie.net>
2017-01-03 19:14:14 +0800

    Removed 'from' and 'message' from Toast

--- app/src/main/java/net/catzie/smsreader/MainActivity.java
+++ app/src/main/java/net/catzie/smsreader/MainActivity.java
@@ -3,33 +3,57 @@ package net.catzie.smsreader;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmsReceiver receiver;
+
+    private TextView tv_sms_from;
+    private TextView tv_sms_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_sms_from = (TextView) findViewById(R.id.tv_sms_from);
+        tv_sms_message = (TextView) findViewById(R.id.tv_sms_message);
+
+        receiver = new SmsReceiver(new Handler()); // create receiver
+        registerReceiver(receiver, new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
+
+//        sendBroadcast(new Intent("android.provider.Telephony.SMS_RECEIVED"));
     }
 
-    public class SmsListener extends BroadcastReceiver {
+    public class SmsReceiver extends BroadcastReceiver {
 
         private SharedPreferences preferences;
-        private String TAG = "SmsListener";
+        private String TAG = "SmsReceiver";
+
+        private final Handler handler; // for executing code on UI thread
+
+        public SmsReceiver(Handler handler){
+            this.handler = handler;
+        }
 
         @Override
         public void onReceive(Context context, Intent intent) {
             // TODO Auto-generated method stub
 
             if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
-                Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+                Bundle bundle = intent.getExtras();
+
+                //---get the SMS message passed in---
                 SmsMessage[] msgs = null;
-                String msg_from;
                 if (bundle != null){
                     //---retrieve the SMS message received---
                     try{
@@ -37,14 +61,24 @@ public class MainActivity extends AppCompatActivity {
                         msgs = new SmsMessage[pdus.length];
                         for(int i=0; i<msgs.length; i++){
                             msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
-                            msg_from = msgs[i].getOriginatingAddress();
-                            String msgBody = msgs[i].getMessageBody();
+                            final String msgFrom = msgs[i].getOriginatingAddress();
+                            final String msgBody = msgs[i].getMessageBody();
 
-//                        Toast.makeText(SmsListener.this, "SMS Received! From: " + msg_from + ", Message: " + msgBody, Toast.LENGTH_LONG).show();
-                            Log.d(TAG, "SMS Received! From: " + msg_from + ", Message: " + msgBody);
+                            Log.d(TAG, "SMS Received! From: " + msgFrom + ", Message: " + msgBody);
+
+                            // post the UI-updating code to our Handler
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this, "SMS Received!", Toast.LENGTH_LONG).show();
+                                    tv_sms_from.setText("From: " + msgFrom);
+                                    tv_sms_message.setText("Message: " + msgBody);
+                                }
+                            });
+
                         }
                     }catch(Exception e){
-//                            Log.d("Exception caught",e.getMessage());
+                            Log.d("Exception caught",e.getMessage());
                     }
                 }
             }
